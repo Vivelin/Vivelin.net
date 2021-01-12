0e74d88103dd9b43b0942eed161be2384079ee15
Vivelin <1766841+Vivelin@users.noreply.github.com>
2021-01-12 14:41:53 +0100

    Added Razor CRUD template for quotes

---
+++ src/Vivelin.Web.Home/Pages/Admin/Quotes/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Vivelin.Web.Data;
+
+namespace Vivelin.Web.Home.Pages.Admin.Quotes
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Vivelin.Web.Data.DataContext _context;
+
+        public CreateModel(Vivelin.Web.Data.DataContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Quote Quote { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Quotes.Add(Quote);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ src/Vivelin.Web.Home/Pages/Admin/Quotes/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Vivelin.Web.Data;
+
+namespace Vivelin.Web.Home.Pages.Admin.Quotes
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Vivelin.Web.Data.DataContext _context;
+
+        public IndexModel(Vivelin.Web.Data.DataContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Quote> Quote { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Quote = await _context.Quotes.ToListAsync();
+        }
+    }
+}
