4cec7fb24e21c35ac1efa8ba8a9f59af30ea57ce
Vivelin <1766841+Vivelin@users.noreply.github.com>
2021-05-20 13:19:04 +0200

    Reworked relative time into approximate time

--- test/Vivelin.Web.Tests/DateTimeExtensionsTests.cs
+++ test/Vivelin.Web.Tests/DateTimeExtensionsTests.cs
@@ -10,75 +10,69 @@ namespace Vivelin.Web.Tests
     public class DateTimeExtensionsTests
     {
         [Theory]
-        [InlineData("00:00:05", "5 seconds ago")]
-        [InlineData("00:05:00", "5 minutes ago")]
-        [InlineData("05:00:00", "5 hours ago")]
-        [InlineData("05:00:00:00", "5 days ago")]
-        [InlineData("-05:00:00:00", "in 5 days")]
-        public void RelativeStringShowsMostSignificantComponent(string timeSpan, string expected)
+        [InlineData("00:00:05", "5 seconds")]
+        [InlineData("00:05:00", "5 minutes")]
+        [InlineData("05:00:00", "5 hours")]
+        [InlineData("05:00:00:00", "5 days")]
+        public void ApproximateStringShowsMostSignificantComponent(string timeSpan, string expected)
         {
             var value = TimeSpan.Parse(timeSpan, CultureInfo.InvariantCulture);
-            var result = value.ToRelativeString(CultureInfo.InvariantCulture);
+            var result = value.ToApproximateString(CultureInfo.InvariantCulture);
             Assert.Equal(expected, result);
         }
 
         [Theory]
-        [InlineData("00:02:30", "2.5 minutes ago")]
-        [InlineData("02:30:00", "2.5 hours ago")]
-        [InlineData("02:12:00:00", "2.5 days ago")]
-        [InlineData("-02:12:00:00", "in 2.5 days")]
-        public void RelativeStringShowsDecimalForSmallerValues(string timeSpan, string expected)
+        [InlineData("00:02:30", "2.5 minutes")]
+        [InlineData("02:30:00", "2.5 hours")]
+        [InlineData("02:12:00:00", "2.5 days")]
+        public void ApproximateStringShowsDecimalForSmallerValues(string timeSpan, string expected)
         {
             var value = TimeSpan.Parse(timeSpan, CultureInfo.InvariantCulture);
-            var result = value.ToRelativeString(CultureInfo.InvariantCulture);
+            var result = value.ToApproximateString(CultureInfo.InvariantCulture);
             Assert.Equal(expected, result);
         }
 
         [Theory]
-        [InlineData("00:02:00", "2 minutes ago")]
-        [InlineData("02:00:00", "2 hours ago")]
-        [InlineData("02:00:00:00", "2 days ago")]
-        [InlineData("-02:00:00:00", "in 2 days")]
-        public void RelativeStringSkipsDecimalForSmallerIntegerValues(string timeSpan, string expected)
+        [InlineData("00:02:00", "2 minutes")]
+        [InlineData("02:00:00", "2 hours")]
+        [InlineData("02:00:00:00", "2 days")]
+        public void ApproximateStringSkipsDecimalForSmallerIntegerValues(string timeSpan, string expected)
         {
             var value = TimeSpan.Parse(timeSpan, CultureInfo.InvariantCulture);
-            var result = value.ToRelativeString(CultureInfo.InvariantCulture);
+            var result = value.ToApproximateString(CultureInfo.InvariantCulture);
             Assert.Equal(expected, result);
         }
 
         [Theory]
-        [InlineData("00:00:07.777", "8 seconds ago")]
-        [InlineData("00:06:12.234", "6 minutes ago")]
-        [InlineData("05:58:32.012", "6 hours ago")]
-        [InlineData("07:04:36:17.663", "7 days ago")]
-        [InlineData("-07:04:36:17.663", "in 7 days")]
-        public void RelativeStringRoundsForLargerValues(string timeSpan, string expected)
+        [InlineData("00:00:07.777", "8 seconds")]
+        [InlineData("00:06:12.234", "6 minutes")]
+        [InlineData("05:58:32.012", "6 hours")]
+        [InlineData("07:04:36:17.663", "7 days")]
+        public void ApproximateStringRoundsForLargerValues(string timeSpan, string expected)
         {
             var value = TimeSpan.Parse(timeSpan, CultureInfo.InvariantCulture);
-            var result = value.ToRelativeString(CultureInfo.InvariantCulture);
+            var result = value.ToApproximateString(CultureInfo.InvariantCulture);
             Assert.Equal(expected, result);
         }
 
         [Theory]
-        [InlineData("00:00:01", "1 second ago")]
-        [InlineData("00:01:00", "1 minute ago")]
-        [InlineData("01:00:00", "1 hour ago")]
-        [InlineData("-01:00:00", "in 1 hour")]
-        public void RelativeStringUsesSingularWhenAppropriate(string timeSpan, string expected)
+        [InlineData("00:00:01", "1 second")]
+        [InlineData("00:01:00", "1 minute")]
+        [InlineData("01:00:00", "1 hour")]
+        public void ApproximateStringUsesSingularWhenAppropriate(string timeSpan, string expected)
         {
             var value = TimeSpan.Parse(timeSpan, CultureInfo.InvariantCulture);
-            var result = value.ToRelativeString(CultureInfo.InvariantCulture);
+            var result = value.ToApproximateString(CultureInfo.InvariantCulture);
             Assert.Equal(expected, result);
         }
 
         [Theory]
-        [InlineData("01:00:00:00", "24 hours ago")]
-        [InlineData("01:12:00:00", "36 hours ago")]
-        [InlineData("-01:00:00:00", "in 24 hours")]
-        public void RelativeStringShowsHoursUnder2Days(string timeSpan, string expected)
+        [InlineData("01:00:00:00", "24 hours")]
+        [InlineData("01:12:00:00", "36 hours")]
+        public void ApproximateStringShowsHoursUnder2Days(string timeSpan, string expected)
         {
             var value = TimeSpan.Parse(timeSpan, CultureInfo.InvariantCulture);
-            var result = value.ToRelativeString(CultureInfo.InvariantCulture);
+            var result = value.ToApproximateString(CultureInfo.InvariantCulture);
             Assert.Equal(expected, result);
         }
     }
