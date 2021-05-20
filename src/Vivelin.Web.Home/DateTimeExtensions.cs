5af5784de9a9a1bb39b10e0c61aab9bc7cee1985
Vivelin <1766841+Vivelin@users.noreply.github.com>
2021-05-20 10:36:41 +0200

    Added singular support

--- src/Vivelin.Web.Home/DateTimeExtensions.cs
+++ src/Vivelin.Web.Home/DateTimeExtensions.cs
@@ -24,11 +24,15 @@ namespace Vivelin.Web.Home
         {
             FormattableString component = value.Duration() switch
             {
+                { TotalSeconds:  1 } x => $"1 second",
                 { TotalSeconds: < 60 } x => $"{Math.Round(x.TotalSeconds)} seconds",
+                { TotalMinutes:  1 } x => $"1 minute",
                 { TotalMinutes: < 5 } x => $"{x.TotalMinutes:0.#} minutes",
                 { TotalMinutes: < 60 } x => $"{Math.Round(x.TotalMinutes)} minutes",
+                { TotalHours: 1 } x => $"1 hour",
                 { TotalHours: < 4 } x => $"{x.TotalHours:0.#} hours",
                 { TotalHours: < 48 } x => $"{Math.Round(x.TotalHours)} hours",
+                { TotalDays: 1 } x => $"1 day",
                 { TotalDays: < 5 } x => $"{x.TotalDays:0.#} days",
                 TimeSpan x => $"{Math.Round(x.TotalDays)} days"
             };
