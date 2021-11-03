efd993fcd7705c7e4b61ff564440ee31811801ae
Laura Verdoes <1766841+Vivelin@users.noreply.github.com>
2021-11-03 13:22:57 +0100

    Added coding guidelines page Added and changed some styles as appropriate for the new page

--- src/Vivelin.Web.Home/MarkdownExtensions/LinkTargetExtension.cs
+++ src/Vivelin.Web.Home/MarkdownExtensions/LinkTargetExtension.cs
@@ -3,6 +3,7 @@
 using Markdig;
 using Markdig.Renderers;
 using Markdig.Renderers.Html;
+using Markdig.Syntax;
 using Markdig.Syntax.Inlines;
 
 namespace Vivelin.Web.Home.MarkdownExtensions
@@ -17,9 +18,25 @@ namespace Vivelin.Web.Home.MarkdownExtensions
         {
             renderer.ObjectWriteBefore += (_, obj) =>
             {
-                if (obj is LinkInline or AutolinkInline)
+                if (IsAbsoluteLink(obj))
                     obj.GetAttributes().AddPropertyIfNotExist("target", "_blank");
+
+                if (obj is AutolinkInline)
+                    obj.GetAttributes().AddClass("autolink");
             };
         }
+
+        private static bool IsAbsoluteLink(MarkdownObject obj)
+        {
+            return obj switch
+            {
+                LinkInline { Url: var url } when IsAbsoluteUrl(url) => true,
+                AutolinkInline { Url: var url } when IsAbsoluteUrl(url) => true,
+                _ => false
+            };
+        }
+
+        private static bool IsAbsoluteUrl(string? url)
+            => url != null && (url.StartsWith("http://") || url.StartsWith("https://") || url.StartsWith("//"));
     }
 }
---
+++ src/Vivelin.Web.Home/Pages/Projects/Code.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Vivelin.Web.Home.Pages.Projects
+{
+    public class CodeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
