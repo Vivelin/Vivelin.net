9737dbb68fc019e2d11645562823b2697e2b9d5e
Laura Verdoes <1766841+Vivelin@users.noreply.github.com>
2021-11-10 16:37:43 +0100

    Added basic text inspector

---
+++ src/Vivelin.Web.Home/Pages/Tools/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Vivelin.Web.Home.Pages.Tools
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/Vivelin.Web.Home/Pages/Tools/Inspector.cshtml.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Unicode;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Vivelin.Web.Home.Pages.Tools
+{
+    public class InspectorModel : PageModel
+    {
+        public void OnGet()
+        {
+            if (HasText)
+            {
+                CodePoints = Text.EnumerateRunes()
+                    .Select(x => new CodePoint(x))
+                    .ToImmutableList();
+            }
+        }
+
+        [FromQuery]
+        public string Text { get; set; }
+
+        public bool HasText => Text != null;
+
+        public IReadOnlyList<CodePoint> CodePoints { get; set; }
+
+        public record CodePoint
+        {
+            public CodePoint(Rune rune)
+            {
+                Rune = rune;
+
+                var charInfo = UnicodeInfo.GetCharInfo(rune.Value);
+                DisplayText = charInfo.GetDisplayText();
+                Name = charInfo.Name ?? charInfo.OldName;
+                CodePointValue = $"U+{rune.Value:X4}";
+                Category = charInfo.Category;
+                Block = charInfo.Block;
+
+                var utf8Bytes = new byte[rune.Utf8SequenceLength];
+                if (rune.TryEncodeToUtf8(utf8Bytes, out var bytesWritten))
+                {
+                    Array.Resize(ref utf8Bytes, bytesWritten);
+                    Utf8 = string.Join(" ", utf8Bytes.Select(x => $"0x{x:X2}"));
+                }
+            }
+
+            public Rune Rune { get; }
+
+            public string CodePointValue { get; }
+
+            public UnicodeCategory Category { get; }
+
+            public string Block { get; }
+
+            public string Name { get; }
+
+            public string DisplayText { get; }
+
+            public string Utf8 { get; }
+        }
+    }
+}
