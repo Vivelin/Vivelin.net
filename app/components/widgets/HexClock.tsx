0f5b1d884b69741af109e844a3119c530319bccd
Laura Verdoes <1766841+Vivelin@users.noreply.github.com>
2024-07-13 19:14:02 +0200

    ✨ Add hexadecimal clock

---
+++ app/components/widgets/HexClock.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import HexTime, { HexTimeProps } from './HexTime';
+import { useTimer } from '~/hooks/useTimer';
+
+export default function HexClock(props: Omit<HexTimeProps, 'date'>) {
+    const [date, setDate] = useState(new Date());
+    useTimer(() => {
+        setDate(new Date());
+    }, 1318);
+
+    return <HexTime date={date} {...props} />;
+}
