b8a5de5107f35aae747091a4745111f9e78165c8
Laura Verdoes <1766841+Vivelin@users.noreply.github.com>
2024-10-20 13:20:15 +0200

    ✨ Add error boundary

--- app/routes/$/route.tsx
+++ app/routes/$/route.tsx
@@ -1,5 +1,11 @@
 import { LoaderFunctionArgs } from '@remix-run/node';
-import { json, MetaFunction, useLoaderData } from '@remix-run/react';
+import {
+    isRouteErrorResponse,
+    json,
+    MetaFunction,
+    useLoaderData,
+    useRouteError,
+} from '@remix-run/react';
 import type { Page } from './page';
 
 export async function loader({ params }: LoaderFunctionArgs) {
@@ -36,9 +42,87 @@ export const meta: MetaFunction<typeof loader> = ({ data }) => {
 export default function Page() {
     const { page } = useLoaderData<typeof loader>();
 
+    // TODO: Generic page metadata in a header/hgroup element? Or maybe separate articles and pages?
     return (
         <main>
             <article dangerouslySetInnerHTML={{ __html: page.content }} />
         </main>
     );
+}
+
+export function ErrorBoundary() {
+    const error = useRouteError();
+
+    if (isRouteErrorResponse(error)) {
+        if (error.status === 404) {
+            return (
+                <main>
+                    <hgroup>
+                        <h1>Error 404</h1>
+                        <p>The page you’re looking for does not exist.</p>
+                    </hgroup>
+                    <p>This is where the 404 page would be, if I had one.</p>
+                </main>
+            );
+        } else {
+            return (
+                <main>
+                    <hgroup>
+                        <h1>Error {error.status}</h1>
+                        <p>{error.statusText}</p>
+                    </hgroup>
+                    <p>
+                        Something went wrong, and we don’t have more specific
+                        error information for you.
+                    </p>
+                </main>
+            );
+        }
+    } else if (
+        error instanceof Error &&
+        process.env.NODE_ENV === 'development'
+    ) {
+        return (
+            <main>
+                <hgroup>
+                    <h1>{error.name}</h1>
+                    <p>{error.message}</p>
+                </hgroup>
+
+                {error.stack && (
+                    <pre>
+                        <samp>{error.stack}</samp>
+                    </pre>
+                )}
+
+                {error.cause instanceof String && <p>Cause: {error.cause}</p>}
+                {error.cause instanceof Error && (
+                    <section>
+                        <hgroup>
+                            <h2>Cause: {error.cause.name}</h2>
+                            <p>{error.cause.message}</p>
+                        </hgroup>
+
+                        {error.cause.stack && (
+                            <pre>
+                                <samp>{error.cause.stack}</samp>
+                            </pre>
+                        )}
+                    </section>
+                )}
+            </main>
+        );
+    } else {
+        return (
+            <main>
+                <hgroup>
+                    <h1>Something went wrong.</h1>
+                </hgroup>
+                <p>
+                    Something went wrong, and we don’t have more specific error
+                    information for you.
+                </p>
+            </main>
+        );
+    }
 }
